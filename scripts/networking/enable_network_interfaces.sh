921979bce382a423e6949811598f841f812ffe1e
Denis Makogon <lildee1991@gmail.com>
2016-06-15 19:24:58 +0300

    Update Vyatta infra blueprint
     - vyatta infra blueprint updated
     - host-to-host blueprint added to showcase how routing happens

---
+++ scripts/networking/enable_network_interfaces.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sex -e
+
+export NUMBER_OF_INTERFACES=`lspci | grep -i eth | wc -l`
+
+for ((i=1;i<$NUMBER_OF_INTERFACES;i++)); do
+    sudo dhclient eth$i 2>/dev/null
+    sudo ifconfig eth$i up 2>/dev/null
+done
+
+sudo sysctl -w net.ipv4.ip_forward=1 &>/dev/null
